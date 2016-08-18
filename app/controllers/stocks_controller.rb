4ccf79d500993d46df16ce48e5cd7fbc7d84bfa6
vbaker007 <vbaker007@hotmail.com>
2016-08-17 22:31:50 -0400

    Form Submission and AJAX: Handle Form Submission and Search w/ AJAX

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,15 +1,36 @@
+#class StocksController < ApplicationController
+
+#  def search
+#      @stock = Stock.find_by_ticker(params[:stock])
+#    end
+
+#    if @stock 
+      #render json: @stock
+#      render partial: 'lookup'
+#    else 
+#      render status: :not_found, nothing: :true
+#  end
+#end
+
 class StocksController < ApplicationController
 
   def search
+
     if params[:stock]
+
       @stock = Stock.find_by_ticker(params[:stock])
+
       @stock ||= Stock.new_from_lookup(params[:stock])
     end
 
-    if @stock 
-      #render json: @stock
+    if @stock
+
       render partial: 'lookup'
-    else 
-      render status: :not_found, nothing: :true
+
+    else
+
+      render status: :not_found, nothing: true
+    end
   end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   get 'my_portfolio', to: "users#my_portfolio"
 
-  get 'search_stocks', to: "stocks#search"
+  get 'search_stocks', to: 'stocks#search'
   
   get 'welcome/new'
 
