44fcb0312fbbb30d47970dfd08acd9f2880510ba
vbaker007 <vbaker007@hotmail.com>
2016-08-21 15:55:56 -0400

    Modify Create Method in User_Stocks Controller

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -24,7 +24,22 @@ class UserStocksController < ApplicationController
   # POST /user_stocks
   # POST /user_stocks.json
   def create
-    @user_stock = UserStock.new(user_stock_params)
+    if params[:stock_id].present?
+      @user_stock = UserStock.new(stock_id: params[:stock_id], user: current_user)
+    else 
+      stock = Stock.find_by_ticker(params[:stock_ticker])
+      if stock
+        @user_stock = UserStock.new(user: current_user, stock: stock)
+      else 
+        stock = Stock.new_from_lookup(params[:stock_ticker])
+        if stock.save
+          @user_stock = UserStock.new(user: current_user, stock: stock)
+        else 
+          @user_stock = nil
+          flash[:error] = "Stock is not available"
+        end
+      end
+    end 
 
     respond_to do |format|
       if @user_stock.save
