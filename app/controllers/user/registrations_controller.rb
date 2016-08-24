ccf94fd4d6c2dc38f34edd98e1d78316330b2d7f
vbaker007 <vbaker007@hotmail.com>
2016-08-24 04:37:06 -0400

    Fix Error in Controllers/Users/Registrations_Controller.rb File

--- app/controllers/user/registrations_controller.rb
+++ app/controllers/user/registrations_controller.rb
@@ -5,6 +5,6 @@ class User::RegistrationsController < Devise::RegistrationsController
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up).push(:first_name, :last_name)
-    devise_parameter_sanitizer.for(:accountant_update).push(:first_name, :last_name)
+    devise_parameter_sanitizer.for(:account_update).push(:first_name, :last_name)
   end
 end
