e0eacf4a7eb00ef19f119c70823dd7a623366d6b
vbaker007 <vbaker007@hotmail.com>
2016-08-24 04:34:16 -0400

    User Details: Adding Attributes to Existing Models

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
-  devise_for :users
+  devise_for :users, :controllers => { :registrations => "user/registrations"}
   resources :user_stocks, except: [:show, :edit, :update]
 
   root 'welcome#index'
